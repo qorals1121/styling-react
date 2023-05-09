e7c28dbebc2992885f6776afd0c5830e6ca1314a
qorals1121 <qorals1121@naver.com>
2023-05-09 23:03:13 +0900

    ~CSS Module 실습

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Component } from 'react';
-import SassComponent from './SassComponent';
+import CSSModule from './CSSModule';
+// import SassComponent from './SassComponent';
+
 
 class App extends Component {
   render() {
     return (
       <div>
-        <SassComponent />
+        <CSSModule />
       </div>
     );
   }
