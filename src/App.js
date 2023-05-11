0b6fe3138687236c6bfd3431fde837e42c62690d
qorals1121 <qorals1121@naver.com>
2023-05-11 23:44:20 +0900

    ~ styled-components 실습

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import StyledComponent from './StyledComponent';
 // import CSSModule from './CSSModule';
 // import SassComponent from './SassComponent';
 
-
 class App extends Component {
   render() {
-    return (
-      <div>
-        <StyledComponent />
-      </div>
+  return (
+    <div>
+       <StyledComponent />
+    </div>
     );
   }
 }
